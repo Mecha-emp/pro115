1f709dad49534a9163ae30c1a746f58f9b290037
Mecha-emp <157562873+Mecha-emp@users.noreply.github.com>
2024-02-13 18:40:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+  
+  
+# Source file path
+source = 'main.txt'
+  
+# destination file path
+dest = 'newfile.txt'
+  
+  
+# Now rename the source path
+# to destination path
+# using os.rename() method
+os.rename(source, dest)
+print("Source path renamed to destination path successfully.")
